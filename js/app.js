071cfec5ec826d45c44ee92d462d62f91e349b6b
sunnywithasmile <112489480+sunnywithasmile@users.noreply.github.com>
2022-12-05 13:27:47 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer"],
+    strings:["","Web Designer","Web Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
